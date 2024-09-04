4f571d408d0e59d5ee5522b66efccce3cba852a4
957011893@qq.com <957011893@qq.com>
2024-09-04 09:37:31 +0800

    update file operation

--- file_operation/file_operation.cpp
+++ file_operation/file_operation.cpp
@@ -8,16 +8,61 @@ FileOperation::FileOperation()
 {
 }
 
-bool FileOperation::ForcePathExistance(const QString &path)
+bool FileOperation::ForcePathExistance(const QString &path) const
 {
     QString temp = path;
-    if (temp[temp.size() - 1] != QChar('/')) temp.append("/");
+    if (temp[temp.size() - 1] != QChar('/')) temp.append("/");  // force to path style
 
     QFileInfo p(temp);
     if (!p.isDir()) {
-        //        qDebug() << __FUNCTION__ << p.absolutePath() << "not found";
         QDir().mkpath(p.absolutePath());
     }
 
     return p.isDir();
+}
+
+QString FileOperation::PathJoin(const QString &p1, const QString &p2) const
+{
+    QString ret = QString("%1/%2").arg(QDir().cleanPath(p1)).arg(QDir().cleanPath(p2));
+    return QDir().cleanPath(ret);
+}
+
+bool FileOperation::FileExistance(const QString &file) const
+{
+    QFileInfo f(file);
+    return f.isFile();
+}
+
+QString FileOperation::AbsCleanFilePath(const QString &file) const
+{
+    QFileInfo f(file);
+    return f.absoluteFilePath();
+}
+
+bool FileOperation::FileCopy(const QString &file, const QString &path) const
+{
+    QFileInfo f(file);
+    if (!f.isFile()) return false;
+    if (!ForcePathExistance(path)) return false;
+
+    QString new_file_path = PathJoin(path, f.fileName());
+    if (FileExistance(new_file_path))
+        if (!FileDelete(new_file_path)) return false;
+
+    return QFile::copy(file, new_file_path);
+}
+
+bool FileOperation::FileCut(const QString &file, const QString &path) const
+{
+    if (!FileCopy(file, path)) return false;
+    return FileDelete(file);
+}
+
+bool FileOperation::FileDelete(const QString &file) const
+{
+    QFileInfo f(file);
+    if (f.isDir()) return false;
+    if (!f.isFile()) return true;
+
+    return QFile::remove(f.absoluteFilePath());
 }
--- file_operation/file_operation.h
+++ file_operation/file_operation.h
@@ -11,7 +11,15 @@ public:
     FileOperation();
 
     // path
-    bool ForcePathExistance(const QString& path);
+    bool ForcePathExistance(const QString& path) const;
+    QString PathJoin(const QString& p1, const QString& p2) const;
+
+    // file
+    bool FileExistance(const QString& file) const;
+    QString AbsCleanFilePath(const QString& file) const;
+    bool FileCopy(const QString& file, const QString& path) const;
+    bool FileCut(const QString& file, const QString& path) const;
+    bool FileDelete(const QString& file) const;
 };
 
 #endif  // FILE_OPERATION_H
