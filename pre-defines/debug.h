2403afdaa8408714354db3f62c10604dc4c5e11e
957011893@qq.com <957011893@qq.com>
2024-12-10 14:53:34 +0800

    add header

--- pre-defines/debug.h
+++ pre-defines/debug.h
@@ -1,9 +1,18 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
 #include <QDebug>
+#include <QThread>
 #include <QDateTime>
 
 #define DBG_FUNC qDebug()<<__FUNCTION__
-#define DBG_FUNC_TIME qDebug()<<__FUNCTION__<<QTime::currentTime().toString("hh:mm:ss.zzz")
-#define DBG_FUNC_DATETIME qDebug()<<__FUNCTION__<<QDateTime::currentDateTime().toString("yyyy-MM-dd_hh:mm:ss.zzz")
 
 #define DBG_TIME qDebug()<<QTime::currentTime().toString("hh:mm:ss.zzz")
 #define DBG_DATETIME qDebug()<<QDateTime::currentDateTime().toString("yyyy-MM-dd_hh:mm:ss.zzz")
+#define DBG_FUNC_TIME qDebug()<<__FUNCTION__<<QTime::currentTime().toString("hh:mm:ss.zzz")
+#define DBG_FUNC_DATETIME qDebug()<<__FUNCTION__<<QDateTime::currentDateTime().toString("yyyy-MM-dd_hh:mm:ss.zzz")
+
+#define DBG_THREAD qDebug()<<QThread::currentThread()
+#define DBG_FUNC_THREAD qDebug()<<__FUNCTION__<<QThread::currentThread()
+
+#endif //DEBUG_H
---
+++ pre-defines/q_prop.h
@@ -0,0 +1,60 @@
+#ifndef Q_PROP_H
+#define Q_PROP_H
+
+///
+#define PROP_READ_WRITE_MEMBER_H(type, lower, UPPER)                         \
+private:                                                                     \
+    Q_PROPERTY(type lower READ lower WRITE set##UPPER NOTIFY lower##Changed) \
+    type m_##lower;                                                          \
+                                                                             \
+public:                                                                      \
+    type lower() const;                                                      \
+    void set##UPPER(type lower);
+
+#define PROP_READ_WRITE_MEMBER_SIGNAL(lower) void lower##Changed();
+
+#define PROP_READ_WRITE_MEMBER_CPP(type, lower, UPPER, CLASS) \
+    type CLASS::lower() const                                 \
+    {                                                         \
+        return m_##lower;                                     \
+    }                                                         \
+    void CLASS::set##UPPER(type lower)                        \
+    {                                                         \
+        m_##lower = lower;                                    \
+        emit lower##Changed();                                \
+    }
+
+///
+#define PROP_READ_QMLWRITE_MEMBER_H(type, lower, UPPER)                      \
+private:                                                                     \
+    Q_PROPERTY(type lower READ lower WRITE set##UPPER NOTIFY lower##Changed) \
+    type m_##lower;                                                          \
+                                                                             \
+public:                                                                      \
+    type lower() const;                                                      \
+    Q_INVOKABLE void set##UPPER(type lower);
+
+#define PROP_READ_WRITE_MEMBER_SIGNAL(lower) void lower##Changed();
+
+#define PROP_READ_WRITE_MEMBER_CPP(type, lower, UPPER, CLASS) \
+    type CLASS::lower() const                                 \
+    {                                                         \
+        return m_##lower;                                     \
+    }                                                         \
+    void CLASS::set##UPPER(type lower)                        \
+    {                                                         \
+        m_##lower = lower;                                    \
+        emit lower##Changed();                                \
+    }
+
+///
+#define PROP_READ_H(type, lower)                            \
+private:                                                    \
+    Q_PROPERTY(type lower READ lower NOTIFY lower##Changed) \
+                                                            \
+public:                                                     \
+    type lower() const;
+
+#define PROP_READ_WRITE_MEMBER_SIGNAL(lower) void lower##Changed();
+
+#endif //Q_PROP_H
