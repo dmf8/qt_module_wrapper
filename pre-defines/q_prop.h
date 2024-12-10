92f077c76d6721bbb6d07138f71949d785b910ae
957011893@qq.com <957011893@qq.com>
2024-12-10 17:56:20 +0800

    handle win and linux

--- pre-defines/q_prop.h
+++ pre-defines/q_prop.h
@@ -1,7 +1,12 @@
 #ifndef Q_PROP_H
 #define Q_PROP_H
 
+#ifdef Q_OS_WIN
+#define PROP_NOTIFY(lower) void lower##Changed();
+#endif
 ///
+
+#ifdef Q_OS_LINUX
 #define PROP_READ_WRITE_MEMBER_H(type, lower, UPPER)                         \
 private:                                                                     \
     Q_PROPERTY(type lower READ lower WRITE set##UPPER NOTIFY lower##Changed) \
@@ -12,6 +17,16 @@ public:                                                                      \
     void set##UPPER(type lower);                                             \
 Q_SIGNALS:                                                                   \
     void lower##Changed();
+#else
+#define PROP_READ_WRITE_MEMBER_H(type, lower, UPPER)                         \
+private:                                                                     \
+    Q_PROPERTY(type lower READ lower WRITE set##UPPER NOTIFY lower##Changed) \
+    type m_##lower;                                                          \
+                                                                             \
+public:                                                                      \
+    type lower() const;                                                      \
+    void set##UPPER(type lower);
+#endif
 
 #define PROP_READ_WRITE_MEMBER_CPP(type, lower, UPPER, CLASS) \
     type CLASS::lower() const                                 \
@@ -25,6 +40,7 @@ Q_SIGNALS:                                                                   \
     }
 
 ///
+#ifdef Q_OS_LINUX
 #define PROP_READ_QMLWRITE_MEMBER_H(type, lower, UPPER)                      \
 private:                                                                     \
     Q_PROPERTY(type lower READ lower WRITE set##UPPER NOTIFY lower##Changed) \
@@ -35,7 +51,16 @@ public:                                                                      \
     Q_INVOKABLE void set##UPPER(type lower);                                 \
 Q_SIGNALS:                                                                   \
     void lower##Changed();
-
+#else
+#define PROP_READ_QMLWRITE_MEMBER_H(type, lower, UPPER)                      \
+private:                                                                     \
+    Q_PROPERTY(type lower READ lower WRITE set##UPPER NOTIFY lower##Changed) \
+    type m_##lower;                                                          \
+                                                                             \
+public:                                                                      \
+    type lower() const;                                                      \
+    Q_INVOKABLE void set##UPPER(type lower);
+#endif
 
 #define PROP_READ_WRITE_MEMBER_CPP(type, lower, UPPER, CLASS) \
     type CLASS::lower() const                                 \
@@ -49,6 +74,7 @@ Q_SIGNALS:                                                                   \
     }
 
 ///
+#ifdef Q_OS_LINUX
 #define PROP_READ_H(type, lower)                            \
 private:                                                    \
     Q_PROPERTY(type lower READ lower NOTIFY lower##Changed) \
@@ -57,5 +83,13 @@ public:                                                     \
     type lower() const;                                     \
 Q_SIGNALS:                                                  \
     void lower##Changed();
+#else
+#define PROP_READ_H(type, lower)                            \
+private:                                                    \
+    Q_PROPERTY(type lower READ lower NOTIFY lower##Changed) \
+                                                            \
+public:                                                     \
+    type lower() const;
+#endif
 
 #endif //Q_PROP_H
